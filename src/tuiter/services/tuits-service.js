b346f9ef6c569a4d61b4949ff1ce51253eff864b
ninest <parth.kabra@gmail.com>
2023-08-07 17:43:45 -0400

    Fix tuits api

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "http://localhost:4000/api/tuits";
+const TUITS_API = process.env.REACT_APP_SERVER_API_URL;
+console.log({ TUITS_API });
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
@@ -19,7 +20,6 @@ export const deleteTuit = async (tid) => {
 };
 
 export const updateTuit = async (tuit) => {
-  const response = await axios
-    .put(`${TUITS_API}/${tuit._id}`, tuit);
+  const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
   return tuit;
-}
+};
